08bfc91198fca9680017a9737d60e64c8d362efb
JumpimgJack <ewgene.shure@gmail.com>
2021-10-28 11:22:39 +0700

    display result

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,19 +14,34 @@ export default {
       rnValue: null,
       rnAct: null,
       messages: {
-        1: "Категория опасности 1: Чрезвычайно опасно для человека",
-        2: "Категория опасности 2: Очень опасно для человека",
-        3: "Категория опасности 3: Опасно для человека",
-        4: "Категория опасности 4: Опасность для человека маловероятна"
+        0: "Категория опасности 5: Опасность для человека очень маловероятна",
+        1: "Категория опасности 4: Опасность для человека маловероятна",
+        2: "Категория опасности 3: Опасно для человека",
+        3: "Категория опасности 2: Очень опасно для человека",
+        4: "Категория опасности 1: Чрезвычайно опасно для человека",
       },
-      danger: null
+      message: '',
+      danger: null,
+      colors: {
+        0: "#009900",
+        1: "#66CC00",
+        2: "#FFFF00",
+        3: "#FF9900",
+        4: "#FF0000"
+      }
     }
   },
   computed: {
     calcActivity: function () {
-      for(let i=0; i<this.rnCalc.length; i++) {     
+      for(let i=0; i<this.rnCalc.length; i++) {
         this.rnAct += this.rnCalc[i].AM/this.rnCalc[i].D_val
       }
+      if(this.rnAct < 0.01) this.danger = 0
+      else if(this.rnAct >= 0.01 && this.rnAct < 1) this.danger = 1
+      else if(this.rnAct >= 1 && this.rnAct < 10) this.danger = 2
+      else if(this.rnAct >= 10 && this.rnAct < 1000) this.danger = 3
+      else if(this.rnAct >= 1000) this.danger = 4
+      this.message = this.messages[this.danger]
       console.log(this.rnAct)
       return this.rnAct
     }
@@ -43,15 +58,21 @@ export default {
           { 
             this.rnCalc.push(JSON.parse(JSON.stringify(this.list[i])))
             console.log(JSON.parse(JSON.stringify(this.list[i])))
+            this.rnValue = null
             return
           }
+/*        else {
+          this.message = "Нет совпадений"
+          this.rnValue = null
+          return
+        }*/
       }
       return this.rnCalc
     },
     displayResult() {
-      this.rnValue = null
-      console.log(this.rnValue)
-      alert(this.rnAct)
+      console.log(this.calcActivity)
+      navigator.clipboard.writeText(this.message)
+      alert(this.calcActivity)
     }
   }
 }
@@ -72,6 +93,10 @@ export default {
         <div v-if="this.rnCalc.length">
           <div class="rn" v-for="item in rnCalc" :key="item.id">
             <p> <span>{{item.Name_RN_Lat}}</span> <span>{{item.D_val}}</span></p>
+          </div>
+          <div class="status">
+            <p class="message"
+              :style="{color: colors[danger]}">{{this.message}}</p>
           </div>
           <button class="submit"
             @click="displayResult">
@@ -102,5 +127,9 @@ span {
   width: 45%;
   padding: 5px 0;
   display: inline-block
+}
+.status {
+  width: 50%;
+  margin: 0 auto;
 }
 </style>
